82dab50f749bcad70ccc81ef59df107985c258c6
Mikael Krief <krief_mikael@hotmail.com>
2017-01-10 13:30:34 +0100

    add solution and task project

---
+++ vsts-release-tasks/vsts-release-tasks/IISDeploy/iisdeploy.ps1
@@ -0,0 +1,23 @@
+﻿[CmdletBinding(DefaultParameterSetName = 'None')]
+param(
+	[string][Parameter(Mandatory=$true)] $Zippath, 
+	[string][Parameter(Mandatory=$true)] $Servername,
+	[string] $Parameterspath
+)
+
+Write-Host "Starting IISDeploy task"
+Trace-VstsEnteringInvocation $MyInvocation
+
+try {
+
+	Write-Verbose "Zippath : $Zippath"
+	Write-Verbose "Servername : $Servername"
+	Write-Verbose "Parameterspath : $Parameterspath"
+
+} catch {
+
+} finally {
+	Trace-VstsLeavingInvocation $MyInvocation
+}
+
+Write-Host "Ending IISDeploy task"
