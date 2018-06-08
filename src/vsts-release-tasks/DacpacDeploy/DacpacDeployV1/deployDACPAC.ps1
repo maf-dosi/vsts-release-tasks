4871feb32e9639ec33e3ba92b9052c6907f4128e
Aloïs AUBIN <alois.aubin@maf.fr>
2018-06-08 21:35:26 +0200

    Correcting logging

--- src/vsts-release-tasks/DacpacDeploy/DacpacDeployV1/deployDACPAC.ps1
+++ src/vsts-release-tasks/DacpacDeploy/DacpacDeployV1/deployDACPAC.ps1
@@ -1,41 +1,66 @@
 ﻿[CmdletBinding()]
 param()
 Trace-VstsEnteringInvocation $MyInvocation
-try {
-    [string]$dacpacFilePath = Get-VstsInput -Name dacpacFilePath -Require
-    [string]$xmlPublishFilePath = Get-VstsInput -Name xmlPublishFilePath
-    [string]$schemaToExclude = Get-VstsInput -Name schemaToExclude
 
-    $user=[Security.Principal.WindowsIdentity]::GetCurrent()
-    Write-Debug "Deploying as  $user.Name"
+function DeployDacPac() {
+    try {
+        [string]$dacpacFilePath = Get-VstsInput -Name dacpacFilePath -Require
+        [string]$xmlPublishFilePath = Get-VstsInput -Name xmlPublishFilePath
+        [string]$schemaToExclude = Get-VstsInput -Name schemaToExclude
 
-    Write-Debug "DACPAC file: $dacpacFilePath"
-    if(-not($xmlPublishFilePath) -or $xmlPublishFilePath -eq $env:SYSTEM_DEFAULTWORKINGDIRECTORY) {
-        $xmlPublishFilePath =  [System.IO.Path]::GetDirectoryName($dacpacFilePath) + "\" + [System.IO.Path]::GetFileNameWithoutExtension($dacpacFilePath) + ".publish.xml"
-    }
-    Write-Debug "Publish profile file: $xmlPublishFilePath"
-    if($schemaToExclude) {
-        Write-Debug "Schema to exclude: $schemaToExclude"
-    }
+        $user = [Security.Principal.WindowsIdentity]::GetCurrent()
+        Write-Host "Deploying as" $user.Name
 
-    $currentDir = (Get-Item -Path ".\" -Verbose).FullName
-    $sqlPackageDir = [System.IO.Path]::Combine($currentDir, "bin")
-    $sqlPackagePath = [System.IO.Path]::Combine($sqlPackageDir, "sqlpackage.exe")
+        Write-Host "DACPAC file: $dacpacFilePath"
+        if (-not($xmlPublishFilePath) -or $xmlPublishFilePath -eq $env:SYSTEM_DEFAULTWORKINGDIRECTORY) {
+            $xmlPublishFilePath = [System.IO.Path]::GetDirectoryName($dacpacFilePath) + "\" + [System.IO.Path]::GetFileNameWithoutExtension($dacpacFilePath) + ".publish.xml"
+        }
+        Write-Host "Publish profile file: $xmlPublishFilePath"
+        Write-Host "Reading connection information from the publish profile"
+        [xml]$publishProfile = Get-Content -Path $xmlPublishFilePath
+        Write-Host "Target connection string:" $publishProfile.Project.PropertyGroup.TargetConnectionString
+        Write-Host "Database name:" $publishProfile.Project.PropertyGroup.TargetDatabaseName
+        if ($schemaToExclude) {
+            Write-Debug "Schema to exclude: $schemaToExclude"
+        }
 
-    $args = @("/Action:Publish", 
+        $currentDir = (Get-Item -Path ".\" -Verbose).FullName
+        $sqlPackageDir = [System.IO.Path]::Combine($currentDir, "bin")
+        $sqlPackagePath = [System.IO.Path]::Combine($sqlPackageDir, "sqlpackage.exe")
+
+        $args = @("/Action:Publish", 
             "/SourceFile:$dacpacFilePath",
             "/Profile:$xmlPublishFilePath")
           
-    if($schemaToExclude) {
-        $args += "/p:AdditionalDeploymentContributors=AgileSqlClub.DeploymentFilterContributor"
-        $args += "/p:AdditionalDeploymentContributorArguments=SqlPackageFilter=IgnoreSchema($schemaToExclude)"
+        if ($schemaToExclude) {
+            $args += "/p:AdditionalDeploymentContributors=AgileSqlClub.DeploymentFilterContributor"
+            $args += "/p:AdditionalDeploymentContributorArguments=SqlPackageFilter=IgnoreSchema($schemaToExclude)"
+        }
+        Write-Debug "Calling $sqlPackagePath with $args"
+        $ErrorActionPreference = 'Continue' 
+        Invoke-Expression "& '$sqlPackagePath' --% $args" 2>&1 -ErrorVariable errors | ForEach-Object {
+            if ($_ -is [System.Management.Automation.ErrorRecord]) {
+                Write-Error $_
+            }
+            else {
+                Write-Host $_
+            }
+        }
+        foreach ($errorMsg in $errors) {
+            Write-Error $errorMsg
+        }
+        $ErrorActionPreference = 'Stop'
+        if ($LASTEXITCODE -ne 0) {
+            throw
+        }
+    } 
+    catch  [System.Exception] {
+        Write-Host "##vso[task.logissue type=error;] DacpacDeploy Error"
+        Write-Host "##vso[task.complete result=Failed]Failed"
     }
-
-    &$sqlPackagePath $args 2>&1
-
-} catch  [System.Exception] {
-    Write-Host "##vso[task.logissue type=error;] DacpacDeploy Error: $_"
-    Write-Host "##vso[task.complete result=Failed]Failed"
-} finally {
-    Write-Host "Ending DacpacDeploy task"
+    finally {
+        Write-Host "Ending DacpacDeploy task"
+    }
 }
+
+DeployDacPac
